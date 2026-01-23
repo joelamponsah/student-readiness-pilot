bb4f77f3abe03e8f8b4a20cdcf55a31b96703fcb
Joel <95248566+joelamponsah@users.noreply.github.com>
2026-01-23 19:30:14 +0100

    Update derived_views.py

--- dashboards/metrics/utils/derived_views.py
+++ dashboards/metrics/utils/derived_views.py
@@ -3,12 +3,14 @@ from utils.insights import apply_insight_engine
 
 def build_user_view(df_fact):
     users = compute_sab_behavioral(df_fact)
-    # ensure institute_std survives:
-    if "institute_std" not in users.columns:
-        users = users.merge(df_fact[["user_id","institute_std"]].drop_duplicates("user_id"),
-                            on="user_id", how="left")
+
+    user_inst = df_fact[['user_id', 'institute_std']].drop_duplicates('user_id')
+    users = users.merge(user_inst, on='user_id', how='left')
+    users['institute_std'] = users['institute_std'].fillna('Unknown').astype(str)
+
     users = apply_insight_engine(users)
     return users
+
 
 def build_test_view(df_fact):
     return compute_test_analytics(df_fact)
